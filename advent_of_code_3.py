93b83368945b6648d5cb1edcec313bc890162903
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-25 20:21:26 +0100

    file completed

--- advent_of_code_3.py
+++ advent_of_code_3.py
@@ -9,21 +9,32 @@ def read_data(path):
     file.close()
     return data
 
-def tree_traverse(data, right):
+def tree_traverse(data, right, down):
     position = 0 # startposition
     tree_count = 0
-    for line in data:
+    
+    for k, line in enumerate(data):
         width = len(line) - 1 # breite von den zeilen
-        letter = line[position]
-        if "#" in letter:
-            tree_count += 1
+        if k % down == 0:
+            # print(k % down)
+            letter = line[position]
+            if "#" in letter:
+                tree_count += 1
 
-        position += right
-        position = position % width  # verschiebe position um mod breite
+            position += right
+            position = position % width  # verschiebe position um mod breite
+
     print(tree_count)
+    return tree_count
 def main():
     data = read_data("advent_of_code_3.txt")
-    tree_traverse(data,3)
+    tree_traverse(data,1,1)
+    tree_traverse(data,3,1)
+    tree_traverse(data,5,1)
+    tree_traverse(data,7,1)
+    tree_traverse(data,1,2)
+    answer = tree_traverse(data,1,1)*tree_traverse(data,3,1)*tree_traverse(data,5,1)*tree_traverse(data,7,1)*tree_traverse(data,1,2)
+    print(answer)
 
 if __name__ == "__main__":
     main()
