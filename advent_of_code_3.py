97b21156a6bed916e28794b7a5fd8fb7c157a9f9
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-25 11:03:49 +0100

    looking for error

--- advent_of_code_3.py
+++ advent_of_code_3.py
@@ -1,19 +1,34 @@
 def read_data(path):
     try:
-        file = open(path,r)
+        file = open(path,"r")
     except:
         print("File", path, "wurde nicht gefunden.")
-    quit()
+        quit()
 
     data = file.readlines()
     file.close()
     return data
 
 def tree_traverse(data):
-    pass
+    # something mod 31 in this case
+    position = 0
+    tree_count = 0
+    interupt = 0
+    for line in data:
+        print(line[position])
+        width = len(line) - 1
+        position += 3
+        position = position % width
+        interupt += 1
+        if line[position] == "#":
+            tree_count += 1
+            print(tree_count)
+            # print(f"---|{line[position]}|---")
+        if interupt == 12:
+            break    
 
 def main():
-    data = read_data()
+    data = read_data("advent_of_code_3.txt")
     tree_traverse(data)
 
 if __name__ == "__main__":
