8d6f7016e80be0fb671bdd97f19a612a2bd58265
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-26 12:46:45 +0100

    dict_list finished

--- advent_of_code_4.py
+++ advent_of_code_4.py
@@ -26,9 +26,8 @@ def listcreater(data, sep, replace_old, replace_new):
     # pid (Passport ID)
     # cid (Country ID)
 
-def checkcredentials(data_list, check_additional_cred = False):
+def checkcredentials(data_list):
     count_part1 = 0
-    count_part2 = 0
     for entry in data_list:
         if ("byr" in entry 
         and "iyr" in entry 
@@ -38,24 +37,31 @@ def checkcredentials(data_list, check_additional_cred = False):
         and "ecl" in entry
         and "pid" in entry):
             count_part1 += 1
-    if check_additional_cred == True:
-        for entry in data_list:
-            if ("byr" in entry 
-            and "iyr" in entry 
-            and "eyr" in entry 
-            and "hgt" in entry
-            and "hcl" in entry
-            and "ecl" in entry
-            and "pid" in entry):
-                pass
-            
-    return count_part1, count_part2
+
+    return count_part1
+
+def create_dict(data_list):
+    # count_break = 0
+    dictlist = []
+    for i, entry in enumerate(data_list):
+        xlist = entry.split(" ") # ['byr:1962', 'pid:547578491', 'eyr:2028', 'ecl:hzl', 'hgt:65in', 'iyr:2013', 'hcl:#623a2f']
+        tuplelist = []
+        for subentry in xlist:
+            ylist = subentry.split(":") # ['hcl', '#623a2f']
+            ytuple = tuple(ylist) # ('hcl', '#623a2f')
+            tuplelist.append(ytuple) # create list of tuples like above
+        dictlist.append(dict(tuplelist)) #  create list of dict-turned lists  
+        # count_break += 1
+        # if count_break == 2:
+        #     break 
+
+    return dictlist
 
 def main():
     data = read_data("advent_of_code_4.txt")
     data_list = listcreater(data, "\n\n", "\n", " ")
-    print(checkcredentials(data_list))
-    # print(len(data_list))
-
+    # print(checkcredentials(data_list))
+    dict_list = create_dict(data_list)
+    print(dict_list)
 if __name__ == "__main__":
     main()    
