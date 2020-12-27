6a0bf2d80d334189d3aa7f9be3a5d72b349c4b85
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-27 04:10:44 +0100

    part1 completed

--- advent_of_code_5.py
+++ advent_of_code_5.py
@@ -22,21 +22,17 @@ def bin_to_dec(bin):
         dec_num += int(binary)*2**(l-count) # turn any binary into decimal
     return dec_num    
 
-def list_to_dec(bin_data, start, end):
-    # for entry in bin_data:
-    #     bin_to_dec(int(entry[:-3]))
-    dec_data = [bin_to_dec(int(entry[start:end])) for entry in bin_data]    
-    return dec_data
-
 def main():
     data = read_data('advent_of_code_5.txt')
     cleaned_data = list_cleaner(data,'\n','') # remove "\n"
     bin_data = list_cleaner(list_cleaner(list_cleaner(list_cleaner(cleaned_data,'B','1'),'F','0'),'L','0'),'R','1') # turn completely into binary
-    dec_data_row = [bin_to_dec(int(entry[:-3])) for entry in bin_data]
-    dec_data_col = [bin_to_dec(int(entry[-3:])) for entry in bin_data]
+    
+    dec_data_row = [bin_to_dec(int(entry[:-3])) for entry in bin_data] # convert till last 3rd position
+    dec_data_col = [bin_to_dec(int(entry[-3:])) for entry in bin_data] # convert from last 3rd position
 
-    seat_id_list = [ a*8 + b for a, b in zip(dec_data_row, dec_data_col)]
+    seat_id_list = [ row*8 + col for row, col in zip(dec_data_row, dec_data_col)]
+    print(max(seat_id_list))
+    print(min(seat_id_list))
 
-    print(seat_id_list)
 if __name__ == "__main__":
     main()     
