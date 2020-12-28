2aba5658e783375490a3ea359839dbe7ac2b0490
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-28 22:59:52 +0100

    part 1 completed part 2 begun

--- advent_of_code_6.py
+++ advent_of_code_6.py
@@ -17,17 +17,31 @@ def listcreater(data, sep, replace_old = None, replace_new = None):
             sep_list[e] = sep_list[e].replace(replace_old, replace_new)
     return sep_list
 
-def count_number(data_list):
+class Dictionary:
+    alphabet = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    length = 26
+    def __init__(self, dictionary):
+        self.dictionary = dictionary
+
+    def dict_creater(self):
+        return    
+
+def count_questions_part1(data_list):
     l = 0
     for entry in data_list:
         l += len(set(entry))
 
     return l    
 
+def count_questions_part2(data_list):
+    l = 0
+    for entry in data_list:
+        len(entry)
+
 def main():
     data = read_data('advent_of_code_6.txt')
     data_list = listcreater(data, '\n\n', '\n', '')
-    print(count_number(data_list))
+    print(count_questions_part1(data_list))
 
 if __name__ == '__main__':
     main()             
