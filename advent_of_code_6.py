254a06fe240fb5126317cccb95eef3143bea7f21
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-28 22:18:34 +0100

    useless git commit

--- advent_of_code_6.py
+++ advent_of_code_6.py
@@ -17,11 +17,17 @@ def listcreater(data, sep, replace_old = None, replace_new = None):
             sep_list[e] = sep_list[e].replace(replace_old, replace_new)
     return sep_list
 
+def count_number(data_list):
+    l = 0
+    for entry in data_list:
+        l += len(set(entry))
+
+    return l    
+
 def main():
     data = read_data('advent_of_code_6.txt')
-    print(data)
-    # data_list = listcreater(data, '\n\n')
-    #print(data_list)
+    data_list = listcreater(data, '\n\n', '\n', '')
+    print(count_number(data_list))
 
 if __name__ == '__main__':
     main()             
