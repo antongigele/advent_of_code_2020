0c39b762763f3b0770dfe67766c00ac80f75a613
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-29 13:47:40 +0100

    dict-class added

--- advent_of_code_6.py
+++ advent_of_code_6.py
@@ -17,15 +17,15 @@ def listcreater(data, sep, replace_old = None, replace_new = None):
             sep_list[e] = sep_list[e].replace(replace_old, replace_new)
     return sep_list
 
-# class Dictionary:
-#     alphabet = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+class Dictionary:
+    alphabet = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-#     def __init__(self, buchstabe):
-#         self.buchstabe = buchstabe
+    def __init__(self, buchstabe = 0):
+        self.buchstabe = buchstabe
 
-#     def dict_creater(self):
-#         self.alphabet_dict = {letter : self.buchstabe for letter in alphabet}
-#         return alphabet_dict
+    def dict_creater(self):
+        alphabet_dict = {letter : self.buchstabe for letter in Dictionary.alphabet}
+        return alphabet_dict
 
 def count_questions_part1(data_list):
     l = 0
@@ -36,16 +36,17 @@ def count_questions_part1(data_list):
 
 def count_questions_part2(data_list):
     l = 0
-    alphabet = ['a','b','c','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     for entry in data_list:
+        mydict = Dictionary().dict_creater()
         len(entry)
-        alphabet_dict = {letter : 0 for letter in alphabet}
+
 
 def main():
     data = read_data('advent_of_code_6.txt')
     data_list = listcreater(data, '\n\n', '\n', '')
     # print(count_questions_part1(data_list))
     mydict = Dictionary('k')
+    mydict = mydict.dict_creater()
     print(mydict)
 
 if __name__ == '__main__':
