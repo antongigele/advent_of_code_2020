c054d97e9abd29badf3091099d0bc6268aa9728b
antongigele <53263067+antongigele@users.noreply.github.com>
2020-12-30 11:56:19 +0100

    many tries

--- advent_of_code_7.sh
+++ advent_of_code_7.sh
@@ -9,8 +9,11 @@ while read line || [ -n "$line" ] ;
     done < advent_of_code_7.txt    
 for ((i=0;i<=20;i++))
     do
+        IFS=' ' read -a arr <<< ${array[i]}
+        string_array[i]=$arr
         # if [[ ${array[i]} == *"shiny gold bags"* ]]; then
         #     num=$(($i + 1))
         #     echo "There it is in line ${num}";
-        # fi    
+        # fi
+        # echo "${string_array[i]}"    
     done
