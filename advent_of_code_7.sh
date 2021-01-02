00abd68919ab1a30166c0482c39ddb34e10b847d
antongigele <53263067+antongigele@users.noreply.github.com>
2021-01-02 22:23:08 +0100

    comments added

--- advent_of_code_7.sh
+++ advent_of_code_7.sh
@@ -5,8 +5,8 @@ while read line || [ -n "$line" ] ;
         line_array[$i]="$line"
         let i++ ;
 
-    done < advent_of_code_7.txt
-    # done < text.txt
+    # done < advent_of_code_7.txt
+    done < text.txt
 # for ((i=0;i<=20;i++))
 #     do
 #         for word in ${line_array[i]}
@@ -32,63 +32,56 @@ for ((i=0;i<=${#line_array[@]};i++))
 # string="muted coral bags contain 1 vibrant teal bag, 5 dim tan bags, 4 light bronze bags." 
 # IFS=' ' read -r -a stringarray <<< "$string"
 
+# array mit den richtigen Taschen erstellen
 for ((i=0;i<=${#shiny_gold_bag_arr[@]};i++))
 do
-    k=0;
     IFS=' ' read -r -a stringarray <<< "${shiny_gold_bag_arr[$i]}"
-    for ((j=0;j<=${#stringarray[@]};j++))
-    do  
-        numplus=$(($j + 1))
-        numplustwo=$(($j + 2))
-        if [[ ${stringarray[$j]} == "shiny" ]] && [[ ${stringarray[$numplus]} == "gold" ]]; then
-            break
-        else
-            valid_bags+=( "${stringarray[$j]} ${stringarray[$numplus]} ${stringarray[$numplustwo]}" )      
-        fi     
-    done
-    ( IFS=$'\n'; echo "${valid_bags[*]}" )
-    
-    for ((l=0;l<=${#valid_bags[@]};l++))
-        do  
-            if [[ "${valid_bags[$l]}" == *"bags" ]] \
-            || [[ "${valid_bags[$l]}" == *"bag" ]] \
-            || [[ "${valid_bags[$l]}" == *"bags," ]] \
-            || [[ "${valid_bags[$l]}" == *"bag," ]] \
-            || [[ "${valid_bags[$l]}" == *"bags." ]] \
-            || [[ "${valid_bags[$l]}" == *"bag." ]]; then
-                valid_bags_cleaned[$k]="${valid_bags[$l]}"
-                let k++;
-            fi   
-        done
-
+    if [[ ${stringarray[1]} == "gold" ]]; then
+        echo "shiny gold bags cannot hold shiny gold bags"
+    else        
+        valid_bags+=( "${stringarray[0]} ${stringarray[1]} ${stringarray[2]}" )
+    fi    
+#    
+    # for ((l=0;l<=${#valid_bags[@]};l++))
+    #     do  
+    #         if [[ "${valid_bags[$l]}" == *"bags" ]] \
+    #         || [[ "${valid_bags[$l]}" == *"bag" ]] \
+    #         || [[ "${valid_bags[$l]}" == *"bags," ]] \
+    #         || [[ "${valid_bags[$l]}" == *"bag," ]] \
+    #         || [[ "${valid_bags[$l]}" == *"bags." ]] \
+    #         || [[ "${valid_bags[$l]}" == *"bag." ]]; then
+    #             valid_bags_cleaned[$k]="${valid_bags[$l]}"
+    #             let k++;
+    #         fi   
+    #     done
 done    
-# ( IFS=$'\n'; echo "${valid_bags[*]}" )
+( IFS=$'\n'; echo "${valid_bags[*]}" )
 # "." "," und "s" müssen vom ende entfernt werden
-for ((l=0;l<=${#valid_bags_cleaned[@]};l++))
+for ((l=0;l<=${#valid_bags[@]};l++))
 do  
-    if [[ "${valid_bags_cleaned[l]: -1}" == "s" ]] \
-    || [[ "${valid_bags_cleaned[l]: -1}" == "," ]] \
-    || [[ "${valid_bags_cleaned[l]: -1}" == "." ]]; then
+    if [[ "${valid_bags[l]: -1}" == "s" ]] \
+    || [[ "${valid_bags[l]: -1}" == "," ]] \
+    || [[ "${valid_bags[l]: -1}" == "." ]]; then
         valid_bags_cleaned[$l]="${valid_bags_cleaned[l]:: -1}";
         # echo "${valid_bags_cleaned[l]}"
     fi    
 done
 # den loop zweimal laufen lassen damit auch die "s" vor dem "," oder "." entfernt werden
-for ((l=0;l<=${#valid_bags_cleaned[@]};l++))
+for ((l=0;l<=${#valid_bags[@]};l++))
 do  
-    if [[ "${valid_bags_cleaned[l]: -1}" == "s" ]] \
-    || [[ "${valid_bags_cleaned[l]: -1}" == "," ]] \
-    || [[ "${valid_bags_cleaned[l]: -1}" == "." ]]; then
-        valid_bags_cleaned[$l]="${valid_bags_cleaned[l]:: -1}";
-        # echo "${valid_bags_cleaned[l]}"
+    if [[ "${valid_bags[l]: -1}" == "s" ]] \
+    || [[ "${valid_bags[l]: -1}" == "," ]] \
+    || [[ "${valid_bags[l]: -1}" == "." ]]; then
+        valid_bags[$l]="${valid_bags[l]:: -1}";
     fi    
 done   
-( IFS=$'\n'; echo "${valid_bags_cleaned[*]}" )
+( IFS=$'\n'; echo "${valid_bags[*]}" )
 
 valid_counter=0;
-for ((l=0;l<${#valid_bags_cleaned[@]};l++))
+# zähl wo überall shiny gold bags reinkommen können
+for ((l=0;l<${#valid_bags[@]};l++))
 do
-    key_regex="${valid_bags_cleaned[l]}";
+    key_regex="${valid_bags[l]}";
     for ((k=0;k<${#line_array[@]};k++))
     do
         if [[ "${line_array[k]}" == *"$key_regex"* ]]; then
@@ -100,5 +93,5 @@ do
 done
 
 echo $valid_counter
-( IFS=$'\n'; echo "${valid_bags_cleaned[*]}" ) > output.txt
+# ( IFS=$'\n'; echo "${valid_bags_cleaned[*]}" ) > output.txt
 # ( IFS=$'\n'; echo "${line_array[*]}" )
