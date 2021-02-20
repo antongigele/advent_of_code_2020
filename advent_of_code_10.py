dc765dc679d88ca27c2452b2739a8434a5363193
Anton Gigele <anton.gigele@aon.at>
2021-02-20 12:11:15 +0100

    part1 finished and part2 begun

--- advent_of_code_10.py
+++ advent_of_code_10.py
@@ -1,3 +1,4 @@
+import math
 def read_data(path):
     try:
         file = open(path, 'r')
@@ -25,20 +26,46 @@ def listcreater(data, sep = None, replace_old = None, replace_new = None):
 #-----------------------part1----------------------#
 
 def climb_up(data):
-    pass
+    list_of_ones = []
+    list_of_threes = [3]
+    int_data = [int(i) for i in data]
+    int_data.sort()
+    if int_data[0] == 1:
+        list_of_ones.append(1)
+    for i in range(len(int_data)-1):
+        if int_data[i+1] - int_data[i] == 1:
+            list_of_ones.append(1)
+        elif int_data[i+1] - int_data[i] == 3:
+            list_of_threes.append(3)
+    return len(list_of_ones) * len(list_of_threes)        
 
-def detect_differences(climb_list):
-    pass
+#-----------------------part2----------------------#
 
+def detect_differences(data):
+    diff_list = []
+    int_data = [int(i) for i in data]
+    int_data.sort()
+    for i in range(len(int_data)-2):
+        if (int_data[i+1] - int_data[i] == 1) and (int_data[i+2] - int_data[i+1] == 1):
+            diff_list.append(1)
+    return len(diff_list)        
 
+def binomial(n):
+    binom_sum = 0
+    for k in range(n):
+        binom_sum += math.binom(n, k)
+    return binom_sum
+
+# math.factorial(n) // math.factorial(k) // math.factorial(n - k)
 
 def main():
     data = read_data("advent_of_code_10.txt")
     cleaned_data = listcreater(data, None, "\n", "")
 #-----------------------part1----------------------#
-
+#    print(climb_up(cleaned_data))
 
 #-----------------------part2----------------------#
+    print(detect_differences(cleaned_data))
 
 if __name__ == "__main__":
     main()
