6c733e2386055d6d9721950f85a8be74fd7fc815
Anton Gigele <anton.gigele@aon.at>
2021-03-09 11:46:07 +0100

    part2 started

--- advent_of_code_11.py
+++ advent_of_code_11.py
@@ -16,8 +16,6 @@ def read_data_as_array(file, datatype = "str", delimiter = "\n"):
     seat_array_bed[1:-1,1:-1] = seat_array
     return seat_array_bed
 
-#-----------------------part1----------------------#
-
 def first_seating_round(seat_array):
     dim = seat_array.shape
     # erste sitzrunde
@@ -26,6 +24,8 @@ def first_seating_round(seat_array):
             if seat_array[i][j] == 0:
                 seat_array[i][j] = 1
     return seat_array            
+
+#-----------------------part1----------------------#
 
 # Adjazenzmatrix eines Sitzshuffles
 def info_array(seat_array):
@@ -87,17 +87,66 @@ def multiple_reshuffle_cycles(previous_cycle, adjacency_matrix):
         return multiple_reshuffle_cycles(next_cycle[0], next_cycle[1])
 
 #-----------------------part2----------------------#
+# Neu schreiben aber ähnlich, für jede Richtung wird eine Schleife benötigt
+def info_array_see(seat_array):
+    dim = seat_array.shape
+    adjacency_matrix = np.full((dim[0], dim[1]), 0, dtype = int)
+    for i in range(1, dim[0]-1):
+        for j in range(1, dim[1]-1):
+            if  seat_array[i][j] != 10:
+                adj_sum = (seat_array[i-1][j]
+                + seat_array[i-1][j+1]
+                + seat_array[i][j+1]
+                + seat_array[i+1][j+1]
+                + seat_array[i+1][j]
+                + seat_array[i+1][j-1]
+                + seat_array[i][j-1]
+                + seat_array[i-1][j-1])
+                adj_sum = int(str(adj_sum)[-1])
+                adjacency_matrix[i][j] = (adj_sum)
+    return adjacency_matrix 
+
+def single_reshuffle_cycle_2(seat_array, adjacency_matrix):
+    dim = seat_array.shape # seat_array und adjacency_matrix haben dieselbe dimension
+    # -ändere die sitzbesetzung anhand der adjazenz-matrix
+    for i in range(1, dim[0]-1):
+        for j in range(1, dim[1]-1):
+            if seat_array[i][j] == 0 and adjacency_matrix[i][j] == 0:
+                seat_array[i][j] = 1
+            elif seat_array[i][j] == 1 and adjacency_matrix[i][j] > 4:
+                seat_array[i][j] = 0
+    # -erstelle eine neue adjazenzmatrix nach der neuen sitzbesetzung
+    new_adjacency_matrix = info_array(seat_array)
+    # -returne die neue sitzbesetzung und die neue adjazenz-matrix
+    return seat_array, new_adjacency_matrix
 
 
+def multiple_reshuffle_cycles_2(previous_cycle, adjacency_matrix):
+    dim = previous_cycle.shape
+    # -einen zyklus durchführen
+    next_cycle = single_reshuffle_cycle_2(previous_cycle, adjacency_matrix)
+    # -zuerst prüfen ob new_adjacency_matrix == adjacency_matrix gilt
+    if areSame(next_cycle[1], adjacency_matrix) == 1:
+        sum = 0
+        for i in range(1, dim[0]-1):
+            for j in range(1, dim[1]-1):
+                if previous_cycle[i][j] == 1:
+                    sum += 1
+        return sum, next_cycle           
+    
+    else:
+        # -zyklusrekursion aufrufen
+        return multiple_reshuffle_cycles_2(next_cycle[0], next_cycle[1])
 
 def main():
-    start_array = read_data_as_array("advent_of_code_11.txt")
-#-----------------------part1----------------------#    
+    start_array = read_data_as_array("test_11.txt")    
     seat_array = first_seating_round(start_array)
-    adjacency_matrix = info_array(seat_array)
-    print(multiple_reshuffle_cycles(seat_array, adjacency_matrix)[0])
+#-----------------------part1----------------------#    
+    # adjacency_matrix = info_array(seat_array)
+    # print(multiple_reshuffle_cycles(seat_array, adjacency_matrix)[0])
 
-#-----------------------part2----------------------#    
-
+#-----------------------part2----------------------#
+    # adjacency_matrix_see = info_array_see(seat_array)
+    print(seat_array)
 if __name__ == "__main__":
     main()    
