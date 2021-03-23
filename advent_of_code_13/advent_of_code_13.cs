be5624886d9f78709e230e4c12d3ff0b68190d77
Anton Gigele <anton.gigele@aon.at>
2021-03-23 15:17:06 +0100

    part1 completed

--- advent_of_code_13/advent_of_code_13.cs
+++ advent_of_code_13/advent_of_code_13.cs
@@ -1,17 +1,17 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace advent_of_code_13 {
     
-    class advent_of_code_13_part1 {
+    class advent_of_code_13 {
 
         public static void Main() {
-            // System.Console.WriteLine(departure_timestamp);
-            (string[] bus_list, float departure_timestamp) = ReadFromFile("advent_of_code_13.txt");
-            var residues = residue_distance(bus_list, departure_timestamp);
-            foreach (double e in residues) {
-                Console.WriteLine(e + "\n");
-            }
+            (string[] bus_list, float departure_timestamp) = ReadFromFile("advent_of_code_13.txt"); // der output dieser funktion ist ein tuple
+            var residues = residue_distance(bus_list, departure_timestamp); // der zeitliche abstand wird als rest kalkuliert
+
+            Console.WriteLine(calculate_min(residues, bus_list));
         }
 
 
@@ -21,36 +21,56 @@ namespace advent_of_code_13 {
             string[] lines = System.IO.File.ReadAllLines(@path);
             float departure_timestamp = Int32.Parse(lines[0]);
 
-            // create the bus array with a separator using the split-method
+            // create das bus array mit einem separator mit der split-methode
             string separator = sep;
             string[] bus_list = lines[1].Split(separator);
 
-            // var bus_tuple = new Tuple<float[], float>(bus_list, departure_timestamp);
             return(bus_list, departure_timestamp);
         }
 
-
+//---------------------------------part1----------------------------------
         public static double[] residue_distance(string[] bus_list, float departure_timestamp) {
             double[] residues = new double[bus_list.Length];
             int i = 0;
             foreach (string e in bus_list) {
-                if (e == "x") 
-                {
-                    Console.WriteLine(e + "\n");
+                if (e == "x") { // ignoriere die x-Einträge
                     residues[i] = 0;
                 }
-                else 
-                {   
-                    Console.WriteLine(departure_timestamp/float.Parse(e) - Math.Floor(departure_timestamp/float.Parse(e)) + "\n");
-                    residues[i] = departure_timestamp/float.Parse(e) - Math.Floor(departure_timestamp/float.Parse(e));
-                    residues[i] *= double.Parse(bus_list[i]);
-                    residues[i] = Math.Floor(residues[i]);
+                else { // dividiere den ersten möglichen abfahrtszeitpunkt durch bus_ids = bus_intervalle und nimm davon nur den betrag der (reste - 1)
+                    residues[i] = 1 - (departure_timestamp/float.Parse(e) - Math.Floor(departure_timestamp/float.Parse(e)));
+                    residues[i] *= double.Parse(bus_list[i]); // multipliziere sie mit den bus_ids
+                    residues[i] = Math.Round(residues[i]);
                 }
                 i++;
             }
             return residues;
         }
+
+        public static int calculate_min(double[] residues, string[] bus_list) {
+            int len = 0;
+            foreach (double e in residues) {
+                if (e != 0) {
+                    len++;
+                }
+            }
+            int[] waiting_times = new int[len];
+            int[] bus_ids = new int[len];
+            int w = 0;
+            int b = 0;
+            for (int i = 0; i < residues.Length; i++) {
+                if (residues[i] != 0) {
+                    waiting_times[w] = (int)residues[i];
+                    w++;
+                    bus_ids[b] = int.Parse(bus_list[i]);
+                    b++;
+                }
+            }
+            int min = waiting_times.Min();
+            int related_bus_id = bus_ids[Array.IndexOf(waiting_times, min)];
+            return related_bus_id*min;
+        }
         
     }
+//---------------------------------part2----------------------------------
 
 }
