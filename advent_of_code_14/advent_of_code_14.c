921cafbebae294bcc498337122f2377ebe8b3544
Anton Gigele <anton.gigele@aon.at>
2021-04-06 12:57:23 +0200

    count_lines function created

--- advent_of_code_14/advent_of_code_14.c
+++ advent_of_code_14/advent_of_code_14.c
@@ -1,6 +1,47 @@
 #include<stdio.h>
+#define MAXCHAR 1000
+
+// char *lines_array (int count, char *path) {
+
+// }
+
+int count_file_len(char *path) {
+    FILE *fp;
+    // char* filename = *path;
+    char c;
+    int count = 1;
+
+    fp = fopen(path, "r");
+    if (fp == NULL){
+        printf("Could not open file %s",path);
+        return 1;
+    }
+    for (c = getc(fp);c != EOF;c = getc(fp)) {
+        if (c == '\n') {
+            count++;
+        }
+    }
+    fclose(fp);
+
+    return count;
+}
 
 int main() {
-    printf("Hello world\n");
+
+    char path[] = "advent_of_code_14.txt";
+    printf("%d\n", count_file_len(path));
+
+    // FILE *fp;
+    // char str[MAXCHAR];
+    // char* filename = "test_14.txt";
+    
+    // fp = fopen(filename, "r");
+    // if (fp == NULL){
+    //     printf("Could not open file %s",filename);
+    //     return 1;
+    // }
+    // while (fgets(str, MAXCHAR, fp) != NULL)
+    //     printf("%s", str);
+    // fclose(fp);
     return 0;
 }
