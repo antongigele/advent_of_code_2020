6fc5212adeb43e682c45ee3562d4c92e0003b75d
Anton Gigele <anton.gigele@aon.at>
2021-04-09 11:39:48 +0200

    sds

--- advent_of_code_14.py
+++ advent_of_code_14.py
@@ -11,6 +11,13 @@ def read_data(path):
 
     return(data)
 
+def dict_list_creater(data):
+    cleaned_data = []
+    for e in data:
+        e = e.replace("\n", "").replace("mem[", "").replace("]", "").replace(" ","")
+        cleaned_data.append(dict([e.split("=")]))
+    return cleaned_data    
+
 def dec_to_bin(decimal): # verwandelt die zahl in binary-format mit führenden nullen
     bin_num = bin(int(decimal))[2:]
     for _ in range(36-len(bin_num)):
@@ -21,7 +28,7 @@ def bin_to_dec(binary): # verwandelt die zahl in decimal
     decimal = int(binary, base = 2)
     return decimal
 
-def apply_mask(mask, decimal): # wendet die maske an
+def apply_mask(mask, decimal): # wendet die maske an, mask ist ein string
     binary_num = dec_to_bin(decimal)
     for i, e in enumerate(mask):
         if e == "X":
@@ -32,11 +39,35 @@ def apply_mask(mask, decimal): # wendet die maske an
             binary_num = binary_num[:i] + e + binary_num[i+1:]
     return binary_num   
 
+def memory_dict(dict_list):
+    mask = ""
+    mem_dict = {}
+    for i in range(len(dict_list)):
+        if "mask" in dict_list[i].keys():
+            mask = dict_list[i].get("mask")
+        else:
+            values_view = dict_list[i].values()
+            value_iterator = iter(values_view)
+            first_value = next(value_iterator)
+            first_key = next(iter(dict_list[i]))
+
+            # print(bin_to_dec(apply_mask(mask, first_value)))
+            # print(first_key)
+            mem_dict.update({first_key : bin_to_dec(apply_mask(mask, first_value))})
+
+    return mem_dict
+
+def sum_memory_values(mem_dict):
+    mem_sum = 0
+    for value in mem_dict.values():
+        mem_sum += value
+    return mem_sum
+
 def main():
-    data = read_data("advent_of_code_14/test_14.txt")
-    print(f"11 = {dec_to_bin('11')}")
-    applied_value = apply_mask("XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X", "11")
-    print(bin_to_dec(applied_value))
+    data = read_data("advent_of_code_14/advent_of_code_14.txt")
+    dict_list = dict_list_creater(data)
+    mem_dict = memory_dict(dict_list)
+    print(sum_memory_values(mem_dict))
 
 if __name__ == "__main__":
     main()    
