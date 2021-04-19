b2c275c4510dc1851a350ee7bfeb1f625e06eb01
Irina Elfantel <irinaelfantel@Irinas-MBP-2.fritz.box>
2021-04-19 18:20:09 +0200

    array function working

--- advent_of_code_15/advent_of_code_15.java
+++ advent_of_code_15/advent_of_code_15.java
@@ -3,18 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+
 public class advent_of_code_15 {
-
-    // public static String readFileAsString(String path) {
-    //     String text = "";
-    // try {
-    //   text = new String(Files.readAllBytes(Paths.get(path)));
-    // } catch (IOException e) {
-    //   e.printStackTrace();
-    // }
-
-    // return text;
-    // }
 
     public static String readFileAsString(String path) {
         File input_file = new File(path);
@@ -33,14 +24,20 @@ public class advent_of_code_15 {
         return "0";
     }
 
-    public static String[] stringToArray(String str) {
-        String[] str_arr = str.split(",",3);
+    public static String[] stringToArray(String str, String sep) {
+        String[] str_arr = str.split(sep);
         return str_arr;
     }
 
+    public static Map progression_dict(String[] input_array) {
+
+    }
+
     public static void main(String[] args) {
-        String str = readFileAsString("test_1.txt");
-        String[] input_array = stringToArray(str);
-        System.out.println(input_array);
+        String string_data = readFileAsString("test_1.txt");
+        String[] input_array;
+        input_array = stringToArray(string_data, ",");
+        System.out.println(Arrays.toString(input_array)); // Arrays.toString Methode notwendig
+
     }
 }
