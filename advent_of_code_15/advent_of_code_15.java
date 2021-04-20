b14366b7d4384e6a67e3752c6586a5b1fae2d3e0
Irina Elfantel <irinaelfantel@Irinas-MBP-2.fritz.box>
2021-04-20 16:20:09 +0200

    put method doesnt work for no reason?

--- advent_of_code_15/advent_of_code_15.java
+++ advent_of_code_15/advent_of_code_15.java
@@ -1,9 +1,11 @@
+import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class advent_of_code_15 {
 
@@ -29,15 +31,51 @@ public class advent_of_code_15 {
         return str_arr;
     }
 
-    public static Map progression_dict(String[] input_array) {
+    public static HashMap<Integer, String> create_dict(String[] input_array) {
+        HashMap<Integer, String> dict = new HashMap<Integer, String>();
+        for(int i = 0; i < input_array.length; i++) {
+            dict.put(i, input_array[i]);
+        }
+        return dict;
+    }
 
+    public static HashMap<Integer, String> eval_last_entry(HashMap<Integer, String> input_dict) {
+        String last_value = input_dict.get(input_dict.size());
+        HashMap<Integer, String> narrowed_input_dict = input_dict;
+        narrowed_input_dict.remove(input_dict.size()-1);
+        if (input_dict.containsValue(last_value) == false) {
+            input_dict.put(input_dict.size()+2, "0");
+        }
+        else {
+                int j = 0;
+            for(String i : narrowed_input_dict.values()) {
+                System.out.println(i);
+                if (i == last_value) {
+                    System.out.println(narrowed_input_dict.get(j));
+                }
+                else {
+                    System.out.println(j);
+                    continue;
+                }
+                j++;
+            }
+        }
+        return input_dict;
     }
 
     public static void main(String[] args) {
+        // File einlesen
         String string_data = readFileAsString("test_1.txt");
+
+        // File-Werte in array umwandeln
         String[] input_array;
         input_array = stringToArray(string_data, ",");
-        System.out.println(Arrays.toString(input_array)); // Arrays.toString Methode notwendig
 
+        // Dictionary erstellen
+        HashMap<Integer, String> dictionary = create_dict(input_array);
+
+        // System.out.println(create_dict(input_array).toString()); // toString Methode notwendig
+        System.out.println(dictionary.toString());
+        System.out.println(eval_last_entry(dictionary));
     }
 }
